9b9ddd3d1ac05905c59f258bd6ce3c23212cf0b1
BC <2449068617@qq.com>
2026-03-03 00:47:41 +0800

    修复部署报错问题：The deploy failed because git init creates a master branch by default, but the push command references main:gh-pages.

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,8 @@ cd docs/.vuepress/github
 git init
 git add -A
 git commit -m 'deploy'
+git branch -M main
 
-git push -f git@github.com:Tskysheep/english-grammar-book.git main:gh-pages
+git push -f https://github.com/Tskysheep/english-grammar-book.git main:gh-pages
 
 cd -
